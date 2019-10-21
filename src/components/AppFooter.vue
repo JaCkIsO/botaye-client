0deefba0d479a4ad3929a9595bfd15f6e69c8b1b
JaCkIsO <samtekblak@gmail.com>
2019-10-21 15:55:29 +0300

    Update AppFooter.vue
    - update social links

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,8 @@
 <template>
   <v-footer color="yellow darken-1" padless dark>
     <v-row justify="center" no-gutters>
-      <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
-        <v-icon size="24px">{{ icon }}</v-icon>
+      <v-btn v-for="icon in icons" :key="icon" :href="icon.link" class="mx-4 white--text" icon>
+        <v-icon size="24px">{{ icon.iconType }}</v-icon>
       </v-btn>
       <v-col class="yellow darken-2 py-4 text-center white--text" cols="12">
         <v-row justify="center" no-gutters>
@@ -20,11 +20,14 @@ export default {
   name: "AppFooter",
   data: () => ({
     icons: [
-      "fab fa-facebook",
-      "fab fa-telegram",
-      "fab fa-instagram",
-      "fab fa-twitter",
-      "fab fa-linkedin"
+      { iconType: "fab fa-facebook", link: "https://fb.me/botayeEt" },
+      { iconType: "fab fa-telegram", link: "https://t.me/botayeET" },
+      {
+        iconType: "fab fa-instagram",
+        link: "https://www.instagram.com/botayeET/"
+      },
+      { iconType: "fab fa-twitter", link: "https://twitter.com/botayeET" },
+      { iconType: "fab fa-linkedin", link: "#" }
     ]
   })
 };
